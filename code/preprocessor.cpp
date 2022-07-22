231f77edbd245ed2039e7d7157e2818a6658ce5a
Gabriel Vieira <gsvieira98@gmail.com>
2022-07-22 18:33:09 -0300

    start preprocessador

--- code/preprocessor.cpp
+++ code/preprocessor.cpp
@@ -1,9 +1,57 @@
-#include "preprocessor.h";
+#include "preprocessor.h"
 
 
 
 
 void readfile(std::fstream& file)
 {
+    std::string line, word;
+    std::istringstream iss;
 
+    while(!file.eof())
+    {
+        getline(file, line);
+        str_toupper(line);
+        removeComment(line);
+        std::istringstream iss(line);
+        std::cout << line << std::endl;
+        // while (iss >> word)
+        // {
+        //     std::cout<< '|' <<word.c_str()<< '|' <<std::endl;
+        // }
+        
+    }
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    std::fstream file(argv[2]);
+    readfile(file);
+}
+
+void str_toupper(std::string& s)
+{
+    std::transform(s.begin(),s.end(),s.begin(),[](unsigned char c){ return std::toupper(c);});
+}
+
+std::string removeFormat(std::string inputstr)
+{
+    size_t comentpos;
+    std::regex str_expr("\\s+");
+    std::string result = std::regex_replace(inputstr,str_expr," ");
+    std::regex str_expr1("^[\\s]+|[\\s]+$|;.*");
+    result = std::regex_replace(result,str_expr1,"");
+    return result;
+}
+
+void removeComment (std::string& line)
+{
+    size_t pos;
+    pos = line.find(';');
+    if(pos != std::string::npos)
+    {
+        line.erase(pos); //ignorar comentários apagando todos os char após o ';'
+    }
 }
--- code/preprocessor.h
+++ code/preprocessor.h
@@ -2,9 +2,16 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include<algorithm>
+#include<regex>
 
 struct preprocessor
 {
     
 };
+void str_toupper(std::string&);
+void readfile(std::fstream&);
+std::string removeFormat(std::string);
+void removeComment (std::string&);
